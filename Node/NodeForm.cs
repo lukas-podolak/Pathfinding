694e2d403c544ba19c544a788026781bb8dbdaac
Lukáš Podolák <50804011+lukas-podolak@users.noreply.github.com>
2020-12-26 14:55:26 +0100

    Create GUI

---
+++ Node/NodeForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Node
+{
+    public class NodeForm
+    {
+    }
+}
