15467a0eb5b33f38895c0d8f06e0ad839888ee33
Lukáš Podolák <50804011+lukas-podolak@users.noreply.github.com>
2020-12-27 15:30:18 +0100

    public void GenerateArea(int size)

--- Pathfinding/Form1.cs
+++ Pathfinding/Form1.cs
@@ -12,9 +12,32 @@ namespace Pathfinding
 {
     public partial class Form1 : Form
     {
+        public UserControl[,] nodeInArea;
+
         public Form1()
         {
             InitializeComponent();
+
+            GenerateArea(5);
+        }
+
+        public void GenerateArea(int size)
+        {
+            nodeInArea = new UserControl[size, size];
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    nodeInArea[x, y] = new UserControl();
+                    nodeInArea[x, y].Width = 800 / size;
+                    nodeInArea[x, y].Height = 800 / size;
+                    nodeInArea[x, y].Top = (y * (800 / size + 1)) + 1;
+                    nodeInArea[x, y].Left = (x * (800 / size + 1)) + 1;
+                    nodeInArea[x, y].BackColor = Color.White;
+                    nodeInArea[x, y].Parent = this;
+                }
+            }
         }
     }
 }
