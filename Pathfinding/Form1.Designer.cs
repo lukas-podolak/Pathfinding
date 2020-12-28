6b7df4d4508adcce3a92847f2a0c246deccb2ebb
Lukáš Podolák <50804011+lukas-podolak@users.noreply.github.com>
2020-12-28 15:55:54 +0100

    Updarte

--- Pathfinding/Form1.Designer.cs
+++ Pathfinding/Form1.Designer.cs
@@ -30,21 +30,36 @@ namespace Pathfinding
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSize = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(340, 331);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(35, 13);
+            this.lblSize.TabIndex = 0;
+            this.lblSize.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.lblSize);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Pathfinding";
+            this.SizeChanged += new System.EventHandler(this.lblSize_SizeChanged);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSize;
     }
 }
 
--- Pathfinding/Form1.cs
+++ Pathfinding/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,26 +19,36 @@ namespace Pathfinding
         {
             InitializeComponent();
 
-            GenerateArea(5);
+            GenerateArea(50);
+
+            lblSize.Text = this.ClientSize.Width + "px W and " + this.ClientSize.Height + " px H";
         }
 
         public void GenerateArea(int size)
         {
             nodeInArea = new UserControl[size, size];
 
+            int width = this.ClientSize.Width - size;
+            int height = this.ClientSize.Height - 2 * size;
+
             for (int x = 0; x < size; x++)
             {
                 for (int y = 0; y < size; y++)
                 {
                     nodeInArea[x, y] = new UserControl();
-                    nodeInArea[x, y].Width = 800 / size;
-                    nodeInArea[x, y].Height = 800 / size;
-                    nodeInArea[x, y].Top = (y * (800 / size + 1)) + 1;
-                    nodeInArea[x, y].Left = (x * (800 / size + 1)) + 1;
+                    nodeInArea[x, y].Width = width / size;
+                    nodeInArea[x, y].Height = height / size;
+                    nodeInArea[x, y].Top = (y * (height / size + 1)) + 1;
+                    nodeInArea[x, y].Left = (x * (width / size + 1)) + 1;
                     nodeInArea[x, y].BackColor = Color.White;
                     nodeInArea[x, y].Parent = this;
                 }
             }
+        }
+
+        private void lblSize_SizeChanged(object sender, EventArgs e)
+        {
+            lblSize.Text = this.ClientSize.Width + "px W and " + this.ClientSize.Height + " px H";
         }
     }
 }
